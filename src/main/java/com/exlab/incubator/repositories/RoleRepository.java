27a1a37404e3558905481eb797c76f66532cf14e
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-11-07 21:49:51 +0300

    Re-added accidentally deleted repository classes.

--- src/main/java/com/exlab/incubator/repositories/RoleRepository.java
+++ src/main/java/com/exlab/incubator/repositories/RoleRepository.java
@@ -4,7 +4,7 @@ import com.exlab.incubator.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-
 }
--- src/main/java/com/exlab/incubator/repositories/SocialNetworkRepository.java
+++ src/main/java/com/exlab/incubator/repositories/SocialNetworkRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SocialNetworkRepository extends JpaRepository<SocialNetwork, Integer> {
+
 }
