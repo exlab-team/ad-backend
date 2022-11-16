f86e0a0db8b10e4f5dc43e8de84e6401188dbc90
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-11-16 13:11:44 +0300

    Changed a System.err.println output to a logger.

--- src/main/java/com/exlab/incubator/configuration/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/exlab/incubator/configuration/jwt/AuthEntryPointJwt.java
@@ -7,20 +7,19 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
-
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        //change to logger
-        System.err.println(authException.getMessage());
+        log.error(authException.getMessage());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
