6f1cb0fbe55e8bf14e8b3883aeb82b66ecc13ccf
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-11-16 14:44:39 +0300

    Added more validation filters to the SignupRequest fields.

--- src/main/java/com/exlab/incubator/dto/requests/SignupRequest.java
+++ src/main/java/com/exlab/incubator/dto/requests/SignupRequest.java
@@ -2,6 +2,7 @@ package com.exlab.incubator.dto.requests;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +14,18 @@ import lombok.ToString;
 @ToString
 public class SignupRequest {
 
+    /*
+        В поле username и password первый и последний знаки, который не должны повторяться.
+    */
+
     @NotBlank
-    @Size(min = 5, max = 30, message = "Username is not correct")
+    @Pattern(regexp = "[0-9a-zA-Z@/_.]{5,30}", message = "Username can contains only letters, numbers and symbols: { @ / _ . }")
+    @Size(min = 5, max = 30, message = "Username size is not correct")
     private String username;
 
     @NotBlank
-    @Size(min = 16, max = 16, message = "Password is not correct")
+    @Size(min = 16, max = 16, message = "Password size is not correct")
+    @Pattern(regexp = "[0-9a-zA-Zа-яА-Я]{16}", message = "Password can contains only letters and numbers")
     private String password;
 
     @NotBlank
