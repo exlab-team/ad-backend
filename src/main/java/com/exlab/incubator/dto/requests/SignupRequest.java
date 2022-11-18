d0a7fd51a7f7bb73560997b380d65252085cde75
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-11-18 17:44:04 +0300

    Configured validation annotations in the SignupRequest class.

--- src/main/java/com/exlab/incubator/dto/requests/SignupRequest.java
+++ src/main/java/com/exlab/incubator/dto/requests/SignupRequest.java
@@ -14,21 +14,18 @@ import lombok.ToString;
 @ToString
 public class SignupRequest {
 
-    /*
-        В поле username и password первый и последний знаки, который не должны повторяться.
-    */
-
     @NotBlank
-    @Pattern(regexp = "[0-9a-zA-Z@/_.]{5,30}", message = "Username can contains only letters, numbers and symbols: { @ / _ . }")
-    @Size(min = 5, max = 30, message = "Username size is not correct")
+    @Size(min = 5, max = 30, message = "Username min size is 5 symbols and max size is 30 symbols")
+    @Pattern(regexp = "[a-zA-Z<>-]{5,30}", message = "Username can contains only letters and symbols: { < > - }")
     private String username;
 
     @NotBlank
-    @Size(min = 16, max = 16, message = "Password size is not correct")
+    @Size(min = 16, max = 16, message = "Password size is 16 symbols")
     @Pattern(regexp = "[0-9a-zA-Zа-яА-Я]{16}", message = "Password can contains only letters and numbers")
     private String password;
 
     @NotBlank
+    @Size(max = 256, message = "Email max size is 256 symbols")
     @Email
     private String email;
 }
