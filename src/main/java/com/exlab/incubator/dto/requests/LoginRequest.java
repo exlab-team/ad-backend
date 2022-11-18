8d546de73d1cdcb32984b34850eb5e56726c829e
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-11-18 17:45:27 +0300

    Configured validation annotations in the LoginRequest class.

--- src/main/java/com/exlab/incubator/dto/requests/LoginRequest.java
+++ src/main/java/com/exlab/incubator/dto/requests/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.exlab.incubator.dto.requests;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +13,12 @@ import lombok.ToString;
 public class LoginRequest {
 
     @NotBlank
-    @Size(min = 5, max = 30, message = "Username is not correct")
+    @Size(min = 5, max = 30, message = "Username min size is 5 symbols and max size is 30 symbols")
+    @Pattern(regexp = "[a-zA-Z<>-]{5,30}", message = "Username can contains only letters and symbols: { < > - }")
     private String username;
 
     @NotBlank
-    @Size(min = 16, max = 16, message = "Password is not correct")
+    @Size(min = 16, max = 16, message = "Password size is 16 symbols")
+    @Pattern(regexp = "[0-9a-zA-Zа-яА-Я]{16}", message = "Password can contains only letters and numbers")
     private String password;
 }
