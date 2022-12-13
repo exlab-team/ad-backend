fc78c94b7b427f35dbd02a2ed64423d59047cc08
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-12-13 17:55:53 +0300

    Deleted annotations from the exceptions

--- src/main/java/com/exlab/incubator/exception/FieldExistsException.java
+++ src/main/java/com/exlab/incubator/exception/FieldExistsException.java
@@ -1,9 +1,6 @@
 package com.exlab.incubator.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class FieldExistsException extends RuntimeException{
 
     public FieldExistsException() {
--- src/main/java/com/exlab/incubator/exception/UserNotFoundException.java
+++ src/main/java/com/exlab/incubator/exception/UserNotFoundException.java
@@ -1,12 +1,10 @@
 package com.exlab.incubator.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException() {
+        super();
     }
 
     public UserNotFoundException(String message) {
