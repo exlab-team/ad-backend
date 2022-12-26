913ad0dace104653bbb85e75811561c4b1cb4621
Yulia <nexai.ylia@gmail.com>
2022-12-26 22:29:39 +0300

    email validator fixed

--- src/main/java/com/exlab/incubator/dto/requests/UserCreateDto.java
+++ src/main/java/com/exlab/incubator/dto/requests/UserCreateDto.java
@@ -1,6 +1,5 @@
 package com.exlab.incubator.dto.requests;
 
-import com.exlab.incubator.validation.EmailConstraint;
 import com.exlab.incubator.validation.PasswordEquality;
 
 import javax.validation.constraints.Email;
@@ -33,22 +32,7 @@ public class UserCreateDto {
     @NotBlank
     @Size(max = 256, message = "Email max size is 256 symbols")
     @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "It allows numeric values from 0 to 9.\n" +
-            "Both uppercase and lowercase letters from a to z are allowed.\n" +
-            "Allowed are underscore “_”, hyphen “-“, and dot “.”\n" +
-            "Dot isn't allowed at the start and end of the local part.\n" +
-            "Consecutive dots aren't allowed.\n" +
-            "For the local part, a maximum of 64 characters are allowed.\n" +
-            "It allows numeric values from 0 to 9.\n" +
-            "We allow both uppercase and lowercase letters from a to z.\n" +
-            "Hyphen “-” and dot “.” aren't allowed at the start and end of the domain part.\n" +
-            "No consecutive dots.")
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "Email can contains only letters, numbers and symbols: { underscore “_”, hyphen “-“, and dot “.”}")
     private String email;
-
-
-//    @NotBlank
-//    @Size(max = 256, message = "Email max size is 256 symbols")
-//    @EmailConstraint
-//    private String email;
 
 }
