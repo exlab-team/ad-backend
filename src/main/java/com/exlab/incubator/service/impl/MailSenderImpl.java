f6f1da3fe46d71dd9b8c2abe520dd858b3c32927
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-02-09 18:34:00 +0300

    Added try-catch block to the mail sender.

--- src/main/java/com/exlab/incubator/service/impl/MailSenderImpl.java
+++ src/main/java/com/exlab/incubator/service/impl/MailSenderImpl.java
@@ -2,13 +2,16 @@ package com.exlab.incubator.service.impl;
 
 
 import com.exlab.incubator.service.MailSender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MailSenderImpl implements MailSender {
     @Autowired
     private JavaMailSender mailSender;
@@ -25,7 +28,11 @@ public class MailSenderImpl implements MailSender {
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
 
-        mailSender.send(mailMessage);
+        try {
+            mailSender.send(mailMessage);
+        } catch (MailSendException e){
+            log.error(e.getMessage());
+        }
     }
 
 }
