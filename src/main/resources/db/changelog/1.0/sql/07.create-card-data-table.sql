7094322883a14c6b816a664c6209c13864874e4b
Yulia <nexai.ylia@gmail.com>
2023-02-15 14:21:24 +0300

    liqubase fix

--- src/main/resources/db/changelog/1.0/sql/07.create-card-data-table.sql
+++ src/main/resources/db/changelog/1.0/sql/07.create-card-data-table.sql
@@ -6,8 +6,8 @@ create table if not exists card_data (
   card_number varchar(16) not null,
   card_owner varchar(30) not null,
   cvv_code int not null,
-  expirationDate varchar(5)not null,
-  total_balance decimal,
+  expiration_date varchar(5)not null,
+  total_balance float,
   constraint card_data_pk primary key(id)
  );
 
