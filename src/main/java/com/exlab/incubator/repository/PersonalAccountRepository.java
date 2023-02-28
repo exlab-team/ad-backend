d3f73dd93fe48bf578394c1c7eb5743280f39cdc
Littledrop <littledrop777@gmail.com>
2023-02-28 17:41:06 +0300

    resolved conflict issues

--- src/main/java/com/exlab/incubator/repository/PersonalAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exlab.incubator.repository;
-
-import com.exlab.incubator.entity.PersonalAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonalAccountRepository extends JpaRepository<PersonalAccount, Long> {
-}
