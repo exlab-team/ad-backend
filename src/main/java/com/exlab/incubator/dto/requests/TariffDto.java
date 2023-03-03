b8f6a1c21ba75ebae3f551f29a30ef1c0c2b2d40
Littledrop <littledrop777@gmail.com>
2023-03-03 13:10:23 +0300

    refactoring. Add some description for swagger.

--- src/main/java/com/exlab/incubator/dto/requests/TariffDto.java
+++ src/main/java/com/exlab/incubator/dto/requests/TariffDto.java
@@ -1,13 +1,12 @@
 package com.exlab.incubator.dto.requests;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 @Value
 public class TariffDto {
 
-   @Schema(description = "one of the names: start, middle, pro")
-   String name;
-   Double price;
+    @Schema(description = "one of the names: start, middle, pro")
+    String name;
+    Double price;
 }
